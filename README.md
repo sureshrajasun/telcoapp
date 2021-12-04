104617e7c70f0f69cfedabd2938c6513e4024e13
Sureshraja <sureshraja.sun@gmail.com>
2021-12-04 22:41:20 +1100

    Updated Readme file also cleaned up the SQL data

--- README.md
+++ README.md
@@ -41,3 +41,49 @@ Use `./gradlew integrationTest` to execute the Integration tests
 ## API Docs reference
 
 Please refer swagger API Docs **http://localhost:8080/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config** for the API contracts
+
+
+## Endpoints
+
+- List all the Phone numbers
+```curl
+curl --location --request GET 'http://localhost:8080/phonenumber'
+curl --location --request GET 'http://localhost:8080/phonenumber?pageNo=0&pageSize=2&sortyBy=id'
+```
+
+- Assign a phone number to a Customer
+
+```curl
+curl --location --request POST 'http://localhost:8080/customer/' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+"id": 1,
+"name": "aaa",
+"email": "ss@ss.com",
+"phoneNumbers": [
+{
+"id": 4,
+"number": "0444444444",
+"status": "ACTIVE"
+}
+]
+}'
+```
+
+- Get all the phone number allocated to a Customer
+
+```curl
+curl --location --request GET 'http://localhost:8080/customer/1' \
+--data-raw ''
+```
+
+- To Activate a phone number
+```curl
+curl --location --request PUT 'http://localhost:8080/phonenumber/6' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+"id": 6,
+"number": "0466666666",
+"status": "ACTIVE"
+}'
+```
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,12 @@
-insert into customer (name, email) values ('aaa', 'ss@ss.com');
+insert into customer (name, email) values ('Jay', 'jay@telco.com');
 
-insert into phoneNumber (number, status) values ('1111','ACTIVE');
-insert into phoneNumber (number, status) values ('22222','ACTIVE');
-insert into phoneNumber (number, status) values ('33333','ACTIVE');
-insert into phoneNumber (number, status) values ('44444','ACTIVE');
-insert into phoneNumber (number, status) values ('555555','ACTIVE');
-insert into phoneNumber (number, status) values ('66666','ACTIVE');
-insert into phoneNumber (number, status) values ('77777','ACTIVE');
+insert into phoneNumber (number, status) values ('0411111111','ACTIVE');
+insert into phoneNumber (number, status) values ('0422222222','ACTIVE');
+insert into phoneNumber (number, status) values ('0433333333','ACTIVE');
+insert into phoneNumber (number, status) values ('0444444444','ACTIVE');
+insert into phoneNumber (number, status) values ('0455555555','ACTIVE');
+insert into phoneNumber (number, status) values ('0466666666','ACTIVE');
+insert into phoneNumber (number, status) values ('0477777777','INACTIVE');
 
 insert into customer_phone values (1, 1);
 insert into customer_phone values (1, 2);
